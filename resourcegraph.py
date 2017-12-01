cf85f30ca160015450ada5542bbd505375125c6b
Harrison <matthew.harrison@44-148.champlain.edu>
2017-12-01 11:49:57 -0500

    moved contents of deadlock to main function in resourcegraph

--- resourcegraph.py
+++ resourcegraph.py
@@ -192,3 +192,22 @@ class ResourceGraph:
             print("Cycle detected at node", hare.name)
             return visited
         return None
+
+def main():
+    # Stub driver
+    resource_graph = ResourceGraph()
+    resource_graph.add_node('A', ['R'], ['S'])
+    resource_graph.add_node('B', [], ['T'])
+    resource_graph.add_node('C', [], ['S'])
+    resource_graph.add_node('D', ['U'], ['S', 'T'])
+    resource_graph.add_node('E', ['T'], ['V'])
+    resource_graph.add_node('F', ['W'], ['S'])
+    resource_graph.add_node('G', ['V'], ['U'])
+
+    resource_graph.display()
+
+    resource_graph.detect_cycles()
+    resource_graph.detect_cycles(override="floyd")
+
+if __name__ == "__main__":
+    main()
